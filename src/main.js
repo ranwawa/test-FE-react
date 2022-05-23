adf315e63b43574ef0005360886f5b2a62feb474
ranwawa <274544338@qq.com>
2022-05-23 09:50:25 +0800

    tototo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).use(Antd).mount('#app');
