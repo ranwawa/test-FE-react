2830b3c60e9b5fec82c431c3139c095886b277ca
ranwawa <274544338@qq.com>
2022-05-24 17:37:57 +0800

    eslint支持

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    <form></form>
+    <a-form :label-col="{ span: 3 }">
+      <a-form-item label="用户名" name="name">
+        <a-input v-model="name" />
+      </a-form-item>
+      <a-form-item label="密码" name="password">
+        <a-input-password v-model="name" />
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   name: 'App',
+  setup() {
+    const age = ref(28);
+
+    return { name: 'rww2', age };
+  },
+  data() {
+    return { name2: 'ranwawa', password: '' };
+  },
 };
 </script>
 
